e2089bbbacc887521852476cfafd9a84b7d7bc64
hmt <tunghoangminh6@gmail.com>
2025-09-20 20:16:44 +0700

    saving model_name

--- src/train.py
+++ src/train.py
@@ -24,7 +24,7 @@ class CliArgs(Tap):
         Tap (Tap): a class for argument parsing.
     """
     
-    model_name: str 
+    model: str 
     batch_size: int = 32
     epochs: int
     learning_rate: float = 0.001
@@ -42,18 +42,18 @@ def main(args: CliArgs):
     """
     
     pass
-    if args.model_name == "TinyVGG":
+    if args.model == "TinyVGG":
         transform = Compose([
             Resize((224, 224)),
             ToTensor(),
         ])
-    elif args.model_name == "ResNet50":
+    elif args.model == "ResNet50":
         transform = torchvision.transforms.Compose([
             torchvision.transforms.Resize((224, 224)),
             torchvision.transforms.ToTensor(),
             torchvision.transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ])
-    elif args.model_name == "EfficientNet":
+    elif args.model == "EfficientNet":
         transform = torchvision.transforms.Compose([
             torchvision.transforms.Resize((288, 288)),
             torchvision.transforms.ToTensor(),
@@ -72,12 +72,12 @@ def main(args: CliArgs):
         train_loader, test_loader, class_names = create_data_loaders(train_dir = "dataset/garbage-dataset/train",
                         test_dir = "dataset/garbage-dataset/test", batch_size=args.batch_size, transforms=transform)
 
-    
-    if args.model_name == "TinyVGG":
+
+    if args.model == "TinyVGG":
         model = TinyVGG(input_shape=(3,224,224), hidden_units = 10, output_shape=len(class_names))
-    elif args.model_name == "ResNet50":
+    elif args.model == "ResNet50":
         model = create_resnet_50_model(num_classes = len(class_names))
-    elif args.model_name == "EfficientNet":
+    elif args.model == "EfficientNet":
         model = create_efficient_net_model(num_classes = len(class_names))
     else:
         model = create_mobile_netv2_model(num_classes = len(class_names))
@@ -89,14 +89,15 @@ def main(args: CliArgs):
     loss_fn = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate)
 
-    log_dir = f"runs/{args.model_name}_{datetime.now().strftime('%Y%m%d-%H%M%S')}"
+    log_dir = f"runs/{args.model}_{datetime.now().strftime('%Y%m%d-%H%M%S')}"
     writer = SummaryWriter(log_dir=log_dir)
 
 
     # Train the model
     train(model, train_loader, test_loader, optimizer, loss_fn, device = device, epochs=args.epochs, writer = writer)
-    torch.save(model.state_dict(), f"{args.model_name}_model.pth")
-    print(f"Model saved to {args.model_name}_model.pth")
+    model_name = (f"{args.model}_model.pth - {args.epochs} epochs, {args.learning_rate} lr, {args.batch_size} batch size, {args.data_ratio} data ratio")
+    torch.save(model.state_dict(), f"{model_name}")
+    print(f"Model saved to {model_name}")
     writer.close()
     
 
