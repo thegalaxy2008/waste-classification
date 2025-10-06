e5b3c06fd557b18fb572aca319731e1055e9244b
hmt <tunghoangminh6@gmail.com>
2025-10-06 22:10:39 +0700

    adding a new dataset

--- src/data_setup.py
+++ src/data_setup.py
@@ -21,47 +21,68 @@ def download_and_prepare_dataset():
     src_folder = os.path.join(path, "garbage-dataset")
     dest_folder = os.path.join(utils.get_cwd(), "dataset", "garbage-dataset")
 
+
+
     # If the folder already exists, skip
+    
+    
     if os.path.exists(dest_folder):
         print(
             f"Dataset already exists at '{dest_folder}'. Skipping download and preparation."
         )
+    else:
+        os.makedirs(os.path.join(dest_folder, "train"), exist_ok=True)
+        os.makedirs(os.path.join(dest_folder, "test"), exist_ok=True)
+
+        split_ratio = 0.8  # 80% train, 20% test
+
+        for class_name in os.listdir(src_folder):
+            class_path = os.path.join(src_folder, class_name)
+            print(f"Processing class '{class_name}' from '{class_path}'")
+            if os.path.isdir(class_path):
+                images = os.listdir(class_path)
+                random.shuffle(images)
+                split_idx = int(len(images) * split_ratio)
+                train_images = images[:split_idx]
+                test_images = images[split_idx:]
+
+                # Create class folders in train and test
+                train_class_dir = os.path.join(dest_folder, "train", class_name)
+                test_class_dir = os.path.join(dest_folder, "test", class_name)
+                os.makedirs(train_class_dir, exist_ok=True)
+                os.makedirs(test_class_dir, exist_ok=True)
+
+                # Copy train images
+                for img in train_images:
+                    shutil.copy2(
+                        os.path.join(class_path, img), os.path.join(train_class_dir, img)
+                    )
+
+                # Copy test images
+                for img in test_images:
+                    shutil.copy2(
+                        os.path.join(class_path, img), os.path.join(test_class_dir, img)
+                    )
+
+
+        print(f"Dataset split into train and test sets at '{dest_folder}'")
+    # Download latest version
+    path_1 = kagglehub.dataset_download("techsash/waste-classification-data")
+
+    print("Path to dataset files:", path_1)
+
+    src_folder_1 = os.path.join(path_1, "DATASET")
+    dest_folder_1 = os.path.join(utils.get_cwd(), "dataset", "recycleable-and-non-recycleable-dataset")
+
+    # If the folder already exists, skip
+    if os.path.exists(dest_folder_1):
+        print(
+            f"Dataset already exists at '{dest_folder_1}'. Skipping download and preparation."
+        )
         return
+    
 
-    os.makedirs(os.path.join(dest_folder, "train"), exist_ok=True)
-    os.makedirs(os.path.join(dest_folder, "test"), exist_ok=True)
-
-    split_ratio = 0.8  # 80% train, 20% test
-
-    for class_name in os.listdir(src_folder):
-        class_path = os.path.join(src_folder, class_name)
-        print(f"Processing class '{class_name}' from '{class_path}'")
-        if os.path.isdir(class_path):
-            images = os.listdir(class_path)
-            random.shuffle(images)
-            split_idx = int(len(images) * split_ratio)
-            train_images = images[:split_idx]
-            test_images = images[split_idx:]
-
-            # Create class folders in train and test
-            train_class_dir = os.path.join(dest_folder, "train", class_name)
-            test_class_dir = os.path.join(dest_folder, "test", class_name)
-            os.makedirs(train_class_dir, exist_ok=True)
-            os.makedirs(test_class_dir, exist_ok=True)
-
-            # Copy train images
-            for img in train_images:
-                shutil.copy2(
-                    os.path.join(class_path, img), os.path.join(train_class_dir, img)
-                )
-
-            # Copy test images
-            for img in test_images:
-                shutil.copy2(
-                    os.path.join(class_path, img), os.path.join(test_class_dir, img)
-                )
-
-    print(f"Dataset split into train and test sets at '{dest_folder}'")
+    shutil.copytree(src_folder_1, dest_folder_1)
 
 
 def create_data_loaders(
@@ -145,3 +166,4 @@ def create_partial_data_loaders(
 
 if __name__ == "__main__":
     download_and_prepare_dataset()
+#cross-import code to not run this 
